8014b23b1a8d543bd8c19ca0f3e8329583297303
zakiuddin786 <zakiuddin321@gmail.com>
2020-04-18 17:22:24 +0530

    bug

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const Post =require('./models/post');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use("/images",express.static(path.join("backend/images")));
+app.use("/images",express.static(path.join("images")));
 
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin","*");
